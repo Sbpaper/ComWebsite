8638247cb4df9aaee0fb17f7a330544f5825e02a
WeiVi <wei_vi@outlook.com>
2020-10-31 13:11:58 +0800

    0

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="layout-Header">
+  <div class="commin-header">
+    <div class="layout">
     <div class="header">
       <div class="w">
-        <div class="title">Header</div>
 
-        <el-dropdown class="user">
-          <span class="el-dropdown-link">
-            更多<i class="el-icon-arrow-down el-icon--right"></i>
-          </span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item @click.native="logoutbutton()">退出登录</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
+        <div class="logo l">
+          <img src="@/assets/img/logo.png" />
+        </div>
+
+        <div class="txtlogo l">
+          <img src="@/assets/img/txt-logo.png" />
+        </div>
+
+        <div class="lien l"></div>
+
+        <div class="description l">看沙雕，上沙雕</div>
+
+        <div class="nav l">
+          <div class="item"><div class="content">发现</div></div>
+          <div class="active"><div class="content">我的订阅</div></div>
+        </div>
+
+        <div class="search">
+          <div class="li">
+            <div class="icon"><img src="@/assets/img/search.png" /></div>
+            <div class="input">
+              <input  placeholder="Serach"/>
+            </div>
+          </div>
+        </div>
+        
+        <div class="userhead r">
+          <div class="ico"><i class="el-icon-arrow-down"></i></div>
+          <div class="img"><img src="@/assets/userhead.png" /></div>
+        </div>
+
+        <div class="icobut r">
+          <div class="img"><img src="@/assets/ico/header-add.png"></div>
+        </div>
+
+        <div class="icobut r">
+          <div class="img"><img src="@/assets/ico/header-msg.png"></div>
+          
+        </div>
+
       </div>
+    </div>
     </div>
   </div>
 </template>
